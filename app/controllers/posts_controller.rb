c0e9f3f704f54286e1c395098bf2bdfc229d8fef
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-01-28 12:03:02 -0200

    Edit and delete buttons added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 	end
 
  	def new
+ 		@post = Post.new
   	end
 
 
@@ -12,13 +13,43 @@ class PostsController < ApplicationController
 	    @post = Post.find(params[:id])
 	end
 
+  	
   	def create
 	    @post = Post.new(post_params)
 	 
-	    @post.save
-	    redirect_to @post
-	  end
+		if @post.save
+	    	redirect_to @post
+	  	else
+	    	render 'new'
+	  	end
+	end
+
  
+ 	def edit
+	    @post = Post.find(params[:id])
+    end
+ 
+
+  	def update
+    	@post = Post.find(params[:id])
+ 
+    	if @post.update(params[:post].permit(:title, :body))
+      		redirect_to @post
+    	else
+      		render 'edit'
+    	end
+  	end
+
+
+  	def destroy
+  	  @post = Post.find(params[:id])
+  	  @post.destroy
+ 
+  	  redirect_to posts_path
+  	end
+
+
+
 	private
 	 
 	def post_params
